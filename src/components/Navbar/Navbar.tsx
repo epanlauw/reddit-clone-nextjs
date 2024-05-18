53ff090a79b1736a4a57ef2a6c4e473fe0b7878b
epanlauw <epanlauw6@gmail.com>
2024-05-18 16:48:15 +0700

    first commit

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Flex, Image } from "@chakra-ui/react";
+import SearchInput from "./SearchInput";
+import RightContent from "./RightContent/RightContent";
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="redditFace" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          alt="redditText"
+          height="46px"
+          display={{ base: "none", md: "unset" }}
+        />
+      </Flex>
+      {/* <Directory /> */}
+      <SearchInput />
+      <RightContent />
+    </Flex>
+  );
+};
+
+export default Navbar;
